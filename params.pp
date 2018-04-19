6a27b959e7929ae60cdee58ffb0a3fe1b423914d
root <root@assadhafeez1.mylabserver.com>
2018-04-19 20:23:02 +0000

    Fix some bugs

--- params.pp
+++ params.pp
@@ -3,7 +3,7 @@ class ntp::params {
  $package_ensure      = 'present'
  $config_name         = 'ntp.conf'
  $config_file_mode    = '0640'
- $server              = ['0.centos.pool.ntp.org','1.centos.pool.ntp.org']
+ $server              = ['0.centos.pool.ntp.org','1.centos.pool.ntp.org','2.centos.pool.ntp.org']
  $service_ensure      = running
  $service_enable      = true
  $service_hasstatus   = true
--- service.pp
+++ service.pp
@@ -1,15 +1,15 @@
 class ntp::service(
   String $service_name        = $::ntp::service_name,
   String $service_ensure      = $::ntp::service_ensure,
-  Boolean $service_enable     = $::ntp::servcie_enable,
+  Boolean $service_enable     = $::ntp::service_enable,
   Boolean $service_hasstatus  = $::ntp::service_hasstatus,
   Boolean $service_hasrestart = $::ntp::service_hasrestart,
 ) {
     
-  service {'ntp':
+  service {'ntpd':
     ensure     =>  $service_ensure,
     enable     =>  $service_enable,
-    hastatus   =>  $service_hasstatus,
+    hasstatus   =>  $service_hasstatus,
     hasrestart => $service_hasrestart,
   }
 }
